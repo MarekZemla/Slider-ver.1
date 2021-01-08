8d1a1f3671275d4bb36c698e0ba699f44dfa112b
Marek Zemla <zemla@marksite4u.pl>
2021-01-08 16:46:15 +0000

    initial commit

--- main.js
+++ main.js
@@ -1,31 +1,55 @@
 const slideList = [{
- img: "images/img1.jpg",
- text: 'Pierwszy tekst'
+    img:"images/bonsai1.jpg",
+    text:'First bonsai'
 },
-{
- img: "images/img2.jpg",
- text: 'Drugi tekst'
-},
-{
- img: "images/img3.jpg",
- text: 'Trzeci tekst'
-}];
+{img:"images/bonsai2.jpg",
+    text:'Second bonsai'},
+{img:"images/bonsai3.jpg",
+    text:'Few bonsai'}];
 
-const image = document.querySelector('img.slider');
-const h1 = document.querySelector('h1.slider');
-// Interfejs
-const time = 3000;
+const time = 2000;
 let active = 0;
 
-const changeSlide = () => {
- active++;
- if (active === slideList.length) {
-  active = 0;
- }
- image.src = slideList[active].img;
- h1.textContent = slideList[active].text;
+const image = document.querySelector('img.slider')
+const h1 = document.querySelector('h1.slider')
+const dots = [...document.querySelectorAll('.dots span')];   
+
+changeDot = ()=>{
+   const activeDot = dots.findIndex(dot => dot.classList.contains('active'));
+   dots[activeDot].classList.remove('active');
+   dots[active].classList.add('active');
+}
+const changeSlide = ()=>{
+    active++;
+    if(active === slideList.length ){
+    active = 0};
+    image.src = slideList[active].img;
+    h1.textContent = slideList[active].text;
+    changeDot();
+}
+let indexInterval = setInterval(changeSlide, time);
+
+const keyChangeSlide = (e)=>{
+    if(e.keyCode == 37 || e.keyCode == 39){
+        clearInterval(indexInterval)
+        e.keyCode == 37 ? active-- : active++
+        if(active == slideList.length ){
+            active = 0;
+            
+        }
+        else if(active < 0){
+            active = slideList.length -1;
+        }
+        console.log('ok')
+        image.src = slideList[active].img;
+        h1.textContent = slideList[active].text;
+        changeDot();
+        indexInterval = setInterval(changeSlide, time)
+
+    }
 
 }
-setInterval(changeSlide, time)
 
-// Implementacje
+
+// utwórz funkcje keyChangeSlide. Zadanie może wymagać także zmian poza funkcją.
+window.addEventListener('keydown', keyChangeSlide)
